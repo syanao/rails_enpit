0672bc82378be927285ad06c58ed7bc00e9af9f1
syanao <kubota.docomo@gmail.com>
2014-09-24 07:11:46 +0000

    Create people scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140923083712) do
+ActiveRecord::Schema.define(version: 20140924065624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20140923083712) do
   create_table "books", force: true do |t|
     t.string   "title"
     t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "people", force: true do |t|
+    t.string   "name"
+    t.integer  "age"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
